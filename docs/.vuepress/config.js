83546f5cebabdfda21efb3676cb4d320ef4bb761
sandy <1445709137@qq.com>
2021-06-23 18:57:14 +0800

    修bug

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,30 +10,4 @@ module.exports = {
     nav: navConf,
     lastUpdated: '上次更新',
   },
-  configureWebpack: () => {
-    const NODE_ENV = process.env.NODE_ENV
-    //判断是否是生产环境
-    if (NODE_ENV === 'production') {
-      return {
-        output: {
-          publicPath: 'https://cdn.jsdelivr.net/gh/jwchan1996/blog@gh-pages/'
-        },
-        resolve: {
-          //配置路径别名
-          alias: {
-            'public': path.resolve(__dirname, './public')
-          }
-        }
-      }
-    } else {
-      return {
-        resolve: {
-          //配置路径别名
-          alias: {
-            'public': path.resolve(__dirname, './public')
-          }
-        }
-      }
-    }
-  }
 };
